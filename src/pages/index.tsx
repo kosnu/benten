378c038b73ac64186b622ed3ceb466010e2d8449
Kosuke Numata <coursek8814@gmail.com>
2022-04-09 17:37:29 +0900

    :recycle: ディレクトリ構成を整理した

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+import { HomePage } from "../ui/home/pages/Home"
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>A4GeTI</title>
+        <meta name="description" content="Application for generate test images"/>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+
+      <HomePage />
+    </div>
+  )
+}
+
+export default Home
