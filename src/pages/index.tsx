bd76c39c452cae07e5678b6f74e7539f76a051a6
Kosuke Numata <coursek8814@gmail.com>
2022-04-11 00:08:04 +0900

    :sparkles: ESLintの設定強化とPrettierの導入
    導入後、設定を反映してコードの変更をしています

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,16 @@ import type { NextPage } from "next"
 import Head from "next/head"
 import { HomePage } from "../ui/home/pages/Home"
 
-const Home: NextPage = () => {
+const Home: NextPage = function () {
   return (
     <div>
       <Head>
         <title>A4GeTI</title>
-        <meta name="description" content="Application for generate test images"/>
-        <link rel="icon" href="/favicon.ico"/>
+        <meta
+          name="description"
+          content="Application for generate test images"
+        />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <HomePage />
