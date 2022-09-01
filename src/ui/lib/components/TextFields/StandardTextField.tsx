45f29ed09d0502640d8aa6edd8540ff2443ba2f6
Kosuke Numata <coursek8814@gmail.com>
2022-09-02 00:03:56 +0900

    :sparkles: 基本のテキストフィールドを作成した

---
+++ src/ui/lib/components/TextFields/StandardTextField.tsx
@@ -0,0 +1,37 @@
+import React, { useId } from "react"
+
+interface StandardTextFieldProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
+  label: string
+  InputProps: React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  >
+}
+
+export function StandardTextField({
+  label,
+  InputProps,
+  ...props
+}: StandardTextFieldProps) {
+  const inputId = useId()
+  return (
+    <>
+      <div {...props} className={"px-1 py-3"}>
+        <label htmlFor={inputId} className={"text-sm text-gray-600"}>
+          {label}
+        </label>
+        <div className={"border-b border-black px-1"}>
+          <input
+            id={inputId}
+            {...InputProps}
+            className={"w-full focus:outline-none"}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
